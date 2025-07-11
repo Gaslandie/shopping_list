157f98629c015ac2b12647881b50051edfef4e31
Mohamed <gassma803@gmail.com>
2025-07-11 12:22:35 +0100

    screen grocery list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/data/dummy_item.dart';
 
 class GroceryList extends StatelessWidget {
   const GroceryList({super.key});
@@ -8,6 +9,19 @@ class GroceryList extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Groceries'),
       ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+        title: Text(groceryItems[index].name),
+        //leading, le contenu avant le title
+        leading: Container(
+          width: 24,
+          height: 24,
+          color: groceryItems[index].category.color,
+        ),
+        //trailing , le contenu après le title
+        trailing: Text(groceryItems[index].quantity.toString()),
+      )),
     );
   }
 }
